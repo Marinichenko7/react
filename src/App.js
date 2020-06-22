6dc49f3170e5a63c1b6fd58d93175e1a78c6ff86
Marinichenko7 <Marinichenko7@gmail.com>
2020-06-22 18:26:37 +0300

    Готовый ссылки с Route и NavLink

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Nav from './Components/Nav/Nav';
 import Profile from './Components/Profile/Profile';
 import Footer from './Components/Footer/Footer';
 import Dialogs from './Components/Dialogs/Dialogs';
+import News from './Components/News/News';
+import Music from './Components/Music/Music';
+import Settings from './Components/Settings/Settings';
 
 const App = () => {
   return (
@@ -23,6 +26,9 @@ const App = () => {
         <div className="content">
           <Route path="/Profile" component={Profile} />
           <Route path="/Dialogs" component={Dialogs} />
+          <Route path="/News" component={News} />
+          <Route path="/Music" component={Music} />
+          <Route path="/Settings" component={Settings} />
         </div>
 
         <div className="footer">
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -8,9 +8,9 @@ const Nav = () => {
             <ul className={st.nav_items}>
                 <li><NavLink to="/Profile" activeClassName={st.active}>Profile</NavLink></li>
                 <li><NavLink to="/Dialogs" activeClassName={st.active}>Messages</NavLink></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Music</a></li>
-                <li><a href="#">Settings</a></li>
+                <li><NavLink to="/News" activeClassName={st.active}>News</NavLink></li>
+                <li><NavLink to="/Music" activeClassName={st.active}>Music</NavLink></li>
+                <li><NavLink to="/Settings" activeClassName={st.active}>Settings</NavLink></li>
             </ul>
         </div>
     )
